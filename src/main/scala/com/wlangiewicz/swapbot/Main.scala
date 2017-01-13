d6828f0ac0904b64af35ea742e8b05bc4e956aee
Wojciech Langiewicz <wlangiewicz@gmail.com>
2017-01-13 22:42:34 +0100

    adapted to new api design

--- src/main/scala/com/wlangiewicz/swapbot/Main.scala
+++ src/main/scala/com/wlangiewicz/swapbot/Main.scala
@@ -4,11 +4,20 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.wlangiewicz.bitmarket.Client
 
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
 object Main extends App {
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
   val client = new Client(Config.privateApiKey, Config.publicApiKey)
+
+  val response = client.performRequest(client.infoRequest).flatMap(client.unmarshalResponse)
+
+
+  println(Await.result(response, 10 seconds))
 
   system.terminate()
 }
